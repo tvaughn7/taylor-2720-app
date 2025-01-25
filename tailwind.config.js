8256b8b965711ad36b2ef69fd12fee76667e6747
Taylor <tvaughn2018@gmail.com>
2025-01-25 12:34:54 -0700

    added daisy UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+
+import daisyui from 'daisyui' 
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,6 +10,11 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    daisyui
+  ],
+  daisyui: {
+    themes: ['light, dark', 'nord', 'winter'],
+  }
 }
 
